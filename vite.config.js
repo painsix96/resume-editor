b99d64f0e7963557dc773c9e35c19df30fab18a7
pain <609510902@qq.com>
2026-04-08 23:47:27 +0800

    Fix deployment issues for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const isGitHubPages = process.env.GITHUB_PAGES === 'true'
-  
-  return {
-    plugins: [react()],
-    base: isGitHubPages ? '/resume-editor/' : '/',
-  }
+export default defineConfig({
+  plugins: [react()],
+  base: '/resume-editor/',
 })

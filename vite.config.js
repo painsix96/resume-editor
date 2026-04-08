4db0d04c66e4ad5f615e7862af49042554ec6a89
pain <609510902@qq.com>
2026-04-09 00:06:00 +0800

    Fix vite config for production build

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/resume-editor/',
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production'
+  
+  return {
+    plugins: [react()],
+    base: isProduction ? '/resume-editor/' : '/',
+  }
 })
